99760710c7c719b898bfc4535574dd0e507a4953
liukun <liu_kun@msn.com>
2017-12-21 22:58:55 +0800

    增加了数据库

--- src/main/java/com/bbz/outsource/uaes/oa/database/DatabaseVercitle.java
+++ src/main/java/com/bbz/outsource/uaes/oa/database/DatabaseVercitle.java
@@ -2,8 +2,62 @@ package com.bbz.outsource.uaes.oa.database;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+import java.sql.ResultSet;
 import java.util.function.Supplier;
 
-public class DatabaseVercitle extends AbstractVerticle {
+public class DatabaseVercitle extends AbstractVerticle{
+  private JDBCClient client;
 
+  public void start(){
+//        JsonObject config = new JsonObject()
+//                .put( "url", "jdbc:mysql://localhost:3306/dlb?useSSL=true" )
+//                .put( "user", "root" )
+//                .put( "password", "root" )
+////                .put( "driverClassName", "com.zaxxer.hikari" )
+//                .put( "maximumPoolSize", 30 );
+
+
+
+    JsonObject config = new JsonObject()
+        .put("provider_class", "io.vertx.ext.jdbc.spi.impl.HikariCPDataSourceProvider")
+        .put("jdbcUrl", "jdbc:mysql://localhost:3306/dlb?useSSL=true")
+        .put("username", "root")
+        .put("password","root")
+//                .put("driverClassName", "org.postgresql.Driver")
+        .put("maximumPoolSize", 30);
+
+    client = JDBCClient.createShared( vertx, config );
+    client.getConnection( res -> {
+      if( res.succeeded() ) {
+
+        SQLConnection connection = res.result();
+
+        connection.query( "SELECT * FROM Rainfall", res2 -> {
+          if( res2.succeeded() ) {
+
+
+
+            for( JsonObject entries : res2.result().getRows() ) {
+              System.out.println( entries );
+            }
+            // 用结果集results进行其他操作
+          }
+        } );
+      } else {
+        // 获取连接失败 - 处理失败的情况
+//                res.cause().printStackTrace();
+      }
+    } );
+  }
+
+  public static void main( String[] args ){
+    Vertx vertx = Vertx.vertx();
+
+
+    vertx.deployVerticle( DatabaseVercitle.class.getName() );
+  }
 }

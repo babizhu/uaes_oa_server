2c39069755cd882e9abc47cc88dc5c609abf21dd
liulaoye <liu_kun@msn.com>
2017-12-25 23:06:51 +0800

    获取插入id

--- src/main/java/com/bbz/outsource/uaes/oa/database/attendance/AttendanceDatabaseService.java
+++ src/main/java/com/bbz/outsource/uaes/oa/database/attendance/AttendanceDatabaseService.java
@@ -1,6 +1,7 @@
 package com.bbz.outsource.uaes.oa.database.attendance;
 
 
+import com.bbz.outsource.uaes.oa.database.IDatabase;
 import io.vertx.codegen.annotations.Fluent;
 import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.codegen.annotations.ProxyGen;
@@ -11,11 +12,12 @@ import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
+
 import java.util.List;
 
 @ProxyGen
 @VertxGen
-public interface AttendanceDatabaseService {
+public interface AttendanceDatabaseService extends IDatabase{
   @GenIgnore
   static AttendanceDatabaseService create(JDBCClient dbClient) {
     return new AttendanceDatabaseServiceImpl(dbClient);
@@ -37,8 +39,16 @@ public interface AttendanceDatabaseService {
 //  AttendanceDatabaseService delete(JsonObject condition, Handler<AsyncResult<List<JsonArray>>> resultHandler);
 //
 //
-//  @Fluent
-//  AttendanceDatabaseService create(JsonObject condition, Handler<AsyncResult<List<JsonArray>>> resultHandler);
+
+  /**
+   * 添加请假记录
+   * @param leave  写入数据库的记录
+   * @param resultHandler resultHandler  返回一个新增记录的ID
+   *
+   * @return              this
+   */
+  @Fluent
+  AttendanceDatabaseService create(JsonArray leave,Handler<AsyncResult<JsonArray>> resultHandler);
 //
 //
 //  @Fluent
--- src/main/java/com/bbz/outsource/uaes/oa/database/attendance/AttendanceDatabaseServiceImpl.java
+++ src/main/java/com/bbz/outsource/uaes/oa/database/attendance/AttendanceDatabaseServiceImpl.java
@@ -7,43 +7,67 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.jdbc.JDBCClient;
 import io.vertx.ext.sql.ResultSet;
+
 import java.util.List;
 
 /**
  * @author liulaoye
  */
-public class AttendanceDatabaseServiceImpl implements AttendanceDatabaseService {
+public class AttendanceDatabaseServiceImpl implements AttendanceDatabaseService{
 
-  private final JDBCClient dbClient;
+    private final JDBCClient dbClient;
 
-  AttendanceDatabaseServiceImpl(JDBCClient dbClient) {
-    this.dbClient = dbClient;
-  }
+    AttendanceDatabaseServiceImpl( JDBCClient dbClient ){
+        this.dbClient = dbClient;
+    }
 
-  @Override
-  public AttendanceDatabaseService search(JsonObject condition, Handler<AsyncResult<List<JsonArray>>> resultHandler) {
-    dbClient.query("select * from ask_for_leave", res -> {
-      if (res.succeeded()) {
-        ResultSet result = res.result();
-        List<JsonArray> pages = result.getResults();
-        resultHandler.handle(Future.succeededFuture(pages));
-      } else {
-        res.cause().printStackTrace();
-      }
-    });
-    return this;
-  }
+    @Override
+    public AttendanceDatabaseService search( JsonObject condition, Handler<AsyncResult<List<JsonArray>>> resultHandler ){
+        dbClient.query( "select * from ask_for_leave", res -> {
+            if( res.succeeded() ) {
+                ResultSet result = res.result();
+                List<JsonArray> pages = result.getResults();
+                resultHandler.handle( Future.succeededFuture( pages ) );
+            } else {
+                res.cause().printStackTrace();
+            }
+        } );
+        return this;
+    }
 
-  @Override
-  public AttendanceDatabaseService query(JsonObject condition,
-      Handler<AsyncResult<String>> resultHandler) {
-    System.out.println(condition);
+    @Override
+    public AttendanceDatabaseService query( JsonObject condition,
+                                            Handler<AsyncResult<String>> resultHandler ){
+        System.out.println( condition );
 
-    resultHandler.handle(Future.succeededFuture("abcdffghkdk"));
+        resultHandler.handle( Future.succeededFuture( "abcdffghkdk" ) );
 
-    return this;
-  }
+        return this;
+    }
+
+    @Override
+    public AttendanceDatabaseService create( JsonArray leave, Handler<AsyncResult<JsonArray>> resultHandler ){
+        String insertSql = "INSERT INTO ask_for_leave (user, begin, approval_result, approvaler) VALUES (?,?,?,?)";
+        dbClient.updateWithParams( insertSql, leave, res -> {
+            if( res.succeeded() ) {
 //
+//                UpdateResult result = res.result();
+//                System.out.println( "Generated keys: " + result.getKeys() );
+//                resultHandler.handle( Future.succeededFuture( result.getKeys() ) );
+                dbClient.query( "SELECT @@INDENTITY as id",res1->{
+                    if(res.succeeded()){
+                        System.out.println(res1.result());
+                    }
+                } );
+
+            } else {
+                resultHandler.handle( Future.failedFuture( res.cause() ) );
+            }
+        } );
+        return this;
+    }
+
+    //
 //  @Override
 //  public AttendanceDatabaseService delete(JsonObject condition,
 //      Handler<AsyncResult<List<JsonArray>>> resultHandler) {
@@ -62,11 +86,11 @@ public class AttendanceDatabaseServiceImpl implements AttendanceDatabaseService 
 //    return this;
 //  }
 //
-  @Override
-  public void doSomething(String str,Handler<AsyncResult<String>> resultHandler) {
-    System.out.println(str);
-    resultHandler.handle(Future.succeededFuture("刘老爷"));
-  }
+    @Override
+    public void doSomething( String str, Handler<AsyncResult<String>> resultHandler ){
+        System.out.println( str );
+        resultHandler.handle( Future.succeededFuture( "刘老爷" ) );
+    }
 
 
 }

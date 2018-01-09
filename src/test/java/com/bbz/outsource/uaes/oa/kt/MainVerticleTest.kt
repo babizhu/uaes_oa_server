3a06c1b3f073be9fad36272c687d10fc82c43e1f
liulaoye <liu_kun@msn.com>
2018-01-09 18:34:16 +0800

    kotlin版本的网站 携程

--- src/test/java/com/bbz/outsource/uaes/oa/kt/MainVerticleTest.kt
+++ src/test/java/com/bbz/outsource/uaes/oa/kt/MainVerticleTest.kt
@@ -3,10 +3,19 @@ package com.bbz.outsource.uaes.oa.kt
 import kotlinx.coroutines.experimental.launch
 import org.junit.Test
 import kotlin.concurrent.thread
-import kotlin.coroutines.experimental.suspendCoroutine
+import kotlin.coroutines.experimental.*
 
 class MainVerticleTest {
-
+    fun asyncCalcMd51(path:String,block: () -> Int){
+        var invoke = block.invoke()
+        println(invoke)
+    }
+    @Test
+    fun t(){
+        asyncCalcMd51("a",{
+            return@asyncCalcMd51 102
+        })
+    }
     @Test
     fun findTusi() {
     }
@@ -34,7 +43,9 @@ class MainVerticleTest {
             cont.resume(2)
         }
     }
-
+    class FilePath(val path: String): AbstractCoroutineContextElement(FilePath){
+        companion object Key : CoroutineContext.Key<FilePath>
+    }
     private suspend fun suspendFunc1(): Int {
         return suspendCoroutine { cont ->
             println(Thread.currentThread())
@@ -47,3 +58,51 @@ class MainVerticleTest {
         }
     }
 }
+
+fun main(args: Array<String>) {
+    println("before coroutine")
+    //启动我们的协程 
+    asyncCalcMd5("test.zip") {
+        println("in coroutine. Before suspend.")
+        //暂停我们的线程，并开始执行一段耗时操作 
+        val result: String = suspendCoroutine {
+            continuation ->
+            println("in suspend block.")
+            continuation.resume(calcMd5(continuation.context[FilePath]!!.path))
+            println("after resume.")
+        }
+        println("in coroutine. After suspend. result = $result")
+    }
+    println("after coroutine")
+}
+/**
+ * 上下文，用来存放我们需要的信息，可以灵活的自定义
+ */
+class FilePath(val path: String): AbstractCoroutineContextElement(FilePath){
+    companion object Key : CoroutineContext.Key<FilePath>
+}
+
+
+fun asyncCalcMd5(path: String, block: suspend () -> Unit) {
+    val continuation = object : Continuation<Unit> {
+        override val context: CoroutineContext
+            get() = FilePath(path)
+
+        override fun resume(value: Unit) {
+            println("resume: $value")
+        }
+
+        override fun resumeWithException(exception: Throwable) {
+            println(exception.toString())
+        }
+    }
+    block.startCoroutine(continuation)
+}
+
+fun calcMd5(path: String): String{
+    println("calc md5 for $path.")
+    //暂时用这个模拟耗时 
+    Thread.sleep(1000)
+    //假设这就是我们计算得到的 MD5 值 
+    return System.currentTimeMillis().toString()
+} 
